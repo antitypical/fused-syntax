dc60efddf175298fc3f181b62053cf554bbd78c0
Rob Rix <rob.rix@me.com>
2019-09-26 10:36:06 -0400

    Define an Ord instance for ScopeT.

--- src/Data/Syntax/Scope.hs
+++ src/Data/Syntax/Scope.hs
@@ -153,6 +153,10 @@ unScopeT (ScopeT s) = s
 instance (RightModule t, Monad f, Eq  a, Eq  b, forall a . Eq  a => Eq  (t f a)) => Eq  (ScopeT a t f b) where
   (==) = (==) `on` fromScopeT
 
+instance (RightModule t, Monad f, Ord a, Ord b, forall a . Eq  a => Eq  (t f a)
+                                              , forall a . Ord a => Ord (t f a)) => Ord (ScopeT a t f b) where
+  compare = compare `on` fromScopeT
+
 instance (Applicative (t f), Applicative f) => Applicative (ScopeT a t f) where
   pure = ScopeT . pure . F . pure
   ScopeT f <*> ScopeT a = ScopeT (liftA2 (liftA2 (<*>)) f a)
