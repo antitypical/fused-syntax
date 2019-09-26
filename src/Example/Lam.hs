20a4ebae0efdd3637e531faf9967c52f8c5c9ce1
Rob Rix <rob.rix@me.com>
2019-09-26 11:08:36 -0400

    Define a smart constructor for abstractions.

--- src/Example/Lam.hs
+++ src/Example/Lam.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Example.Lam
 ( Lam(..)
+, lam
 , ($$)
 ) where
 
@@ -13,6 +14,9 @@ data Lam t a
 
 infixl 9 :$
 
+
+lam :: (Eq a, Has Lam sig t) => a -> t a -> t a
+lam v b = term (Abs (abstract1 v b))
 
 ($$) :: Has Lam sig t => t a -> t a -> t a
 f $$ a = term (f :$ a)
