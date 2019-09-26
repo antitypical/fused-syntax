3562de950e724553b9c21be197cfff6e6e12fad2
Rob Rix <rob.rix@me.com>
2019-09-26 10:03:11 -0400

    Move incr up.

--- src/Data/Syntax/Scope.hs
+++ src/Data/Syntax/Scope.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE DeriveTraversable, LambdaCase #-}
 module Data.Syntax.Scope
 ( Incr(..)
+, incr
 , matchEither
 , matchMaybe
-, incr
 ) where
 
 data Incr a b
@@ -20,11 +20,11 @@ instance Monad (Incr a) where
   Z a >>= _ = Z a
   S a >>= f = f a
 
+incr :: (a -> c) -> (b -> c) -> Incr a b -> c
+incr z s = \case { Z a -> z a ; S b -> s b }
+
 matchEither :: Applicative f => (b -> Either a c) -> b -> Incr a (f c)
 matchEither f x = either Z (S . pure) (f x)
 
 matchMaybe :: (b -> Maybe a) -> (b -> Either a b)
 matchMaybe f a = maybe (Right a) Left (f a)
-
-incr :: (a -> c) -> (b -> c) -> Incr a b -> c
-incr z s = \case { Z a -> z a ; S b -> s b }
