ec6829f24928a9d1b01a0a2796ceaf9cac793b9d
Rob Rix <rob.rix@me.com>
2019-09-26 10:48:32 -0400

    Derive a Show instance for Term.

--- src/Data/Syntax/Term.hs
+++ src/Data/Syntax/Term.hs
@@ -24,6 +24,7 @@ deriving instance ( Ord a
                                                 , forall y . Ord y => Ord (g y)) => Ord (sig g x)
                   )
                => Ord (Term sig a)
+deriving instance (Show a, forall g x . (Show x, forall y . Show y => Show (g y)) => Show (sig g x)) => Show (Term sig a)
 
 deriving instance ( forall g . Foldable    g => Foldable    (sig g)) => Foldable    (Term sig)
 deriving instance ( forall g . Functor     g => Functor     (sig g)) => Functor     (Term sig)
