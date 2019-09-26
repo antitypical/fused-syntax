85b959dc8ac11966c5b4d6412007115d83536a8f
Rob Rix <rob.rix@me.com>
2019-09-26 11:01:29 -0400

    Define a smart constructor for applications.

--- src/Example/Lam.hs
+++ src/Example/Lam.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Example.Lam
 ( Lam(..)
+, ($$)
 ) where
 
+import Data.Syntax.Algebra
 import Data.Syntax.Scope
 
 data Lam t a
   = Abs (Scope () t a)
   | App (t a) (t a)
+
+($$) :: Has Lam sig t => t a -> t a -> t a
+f $$ a = term (App f a)
+
+infixl 9 $$
