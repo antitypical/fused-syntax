aa6f8bb82f116394402084250af87c8b196e455c
Rob Rix <rob.rix@me.com>
2019-09-26 09:17:58 -0400

    Define an HFunctor class.

--- src/Data/Syntax/Functor.hs
+++ src/Data/Syntax/Functor.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Data.Syntax.Functor
-() where
+( HFunctor(..)
+) where
+
+-- | Higher-order functors of kind @(* -> *) -> (* -> *)@ map functors to functors.
+class HFunctor h where
+  -- | Higher-order functor map of a natural transformation over higher-order positions within the syntax.
+  hmap :: Functor m => (forall x . m x -> n x) -> (h m a -> h n a)
