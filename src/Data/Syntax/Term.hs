5e0f5797dba66c312b8fc1c9a94b8383ee21b32c
Rob Rix <rob.rix@me.com>
2019-09-26 10:44:35 -0400

    Define Applicative & Monad instances for Term.

--- src/Data/Syntax/Term.hs
+++ src/Data/Syntax/Term.hs
@@ -3,8 +3,25 @@ module Data.Syntax.Term
 ( Term(..)
 ) where
 
+import Control.Monad (ap)
+import Control.Monad.Module
+
 data Term sig a
   = Var a
   | Term (sig (Term sig) a)
 
 deriving instance ( forall g . Functor     g => Functor     (sig g)) => Functor     (Term sig)
+
+instance ( RightModule sig
+         , forall f . Functor f => Functor (sig f)
+         )
+      => Applicative (Term sig) where
+  pure = Var
+  (<*>) = ap
+
+instance ( RightModule sig
+         , forall f . Functor f => Functor (sig f)
+         )
+      => Monad (Term sig) where
+  Var  a >>= f = f a
+  Term t >>= f = Term (t >>=* f)
