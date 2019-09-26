027e4cf3f599a4dff42b9e7ea4494f87b6ca77a2
Rob Rix <rob.rix@me.com>
2019-09-26 09:45:57 -0400

    Define an Applicative instance for Incr.

--- src/Data/Syntax/Scope.hs
+++ src/Data/Syntax/Scope.hs
@@ -7,3 +7,8 @@ data Incr a b
   = Z a
   | S b
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Applicative (Incr a) where
+  pure = S
+  Z a <*> _ = Z a
+  S f <*> a = f <$> a
