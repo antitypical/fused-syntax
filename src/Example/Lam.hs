24df137ae3abf01350e52f7ce8a3923c3d676377
Rob Rix <rob.rix@me.com>
2019-09-26 11:01:44 -0400

    Rename the App constructor.

--- src/Example/Lam.hs
+++ src/Example/Lam.hs
@@ -9,9 +9,9 @@ import Data.Syntax.Scope
 
 data Lam t a
   = Abs (Scope () t a)
-  | App (t a) (t a)
+  | t a :$ t a
 
 ($$) :: Has Lam sig t => t a -> t a -> t a
-f $$ a = term (App f a)
+f $$ a = term (f :$ a)
 
 infixl 9 $$
