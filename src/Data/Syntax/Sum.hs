5ac7c0f1765461675c67a7c059de3cc63978c155
Rob Rix <rob.rix@me.com>
2019-09-26 09:20:51 -0400

    Derive an HFunctor instance for :+:.

--- src/Data/Syntax/Sum.hs
+++ src/Data/Syntax/Sum.hs
@@ -3,9 +3,12 @@ module Data.Syntax.Sum
 ( (:+:)(..)
 ) where
 
+import Data.Syntax.Functor
 import GHC.Generics (Generic, Generic1)
 
 data (f :+: g) t a
   = L (f t a)
   | R (g t a)
   deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
+
+instance (HFunctor f, HFunctor g) => HFunctor (f :+: g)
