22ca524158265ff46e03c5e6f3cdc8cb8cbf4ef3
Rob Rix <rob.rix@me.com>
2019-09-26 10:52:31 -0400

    Define a constraint synonym for membership + an algebra.

--- src/Data/Syntax/Algebra.hs
+++ src/Data/Syntax/Algebra.hs
@@ -1,10 +1,15 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ConstraintKinds, FunctionalDependencies #-}
 module Data.Syntax.Algebra
 ( Algebra(..)
+, Has
 ) where
 
 import Data.Syntax.Functor
+import Data.Syntax.Sum
 
 class HFunctor syntax => Algebra syntax carrier | carrier -> syntax where
   gen :: a -> carrier a
   alg :: syntax carrier a -> carrier a
+
+
+type Has syn sig t = (Inject syn sig, Algebra sig t)
