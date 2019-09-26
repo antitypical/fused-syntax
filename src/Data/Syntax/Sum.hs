980ef2408b3ba133b0f0ef8e3a04e1bcbaee9fbe
Rob Rix <rob.rix@me.com>
2019-09-26 09:20:13 -0400

    Derive Generic & Generic1 instances for :+:.

--- src/Data/Syntax/Sum.hs
+++ src/Data/Syntax/Sum.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE DeriveTraversable, PolyKinds, TypeOperators #-}
+{-# LANGUAGE DeriveGeneric, DeriveTraversable, PolyKinds, TypeOperators #-}
 module Data.Syntax.Sum
 ( (:+:)(..)
 ) where
 
+import GHC.Generics (Generic, Generic1)
+
 data (f :+: g) t a
   = L (f t a)
   | R (g t a)
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+  deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
