cfa016de145daa2795b5faf81a24d6d4ec6b09bb
Rob Rix <rob.rix@me.com>
2019-09-26 09:48:40 -0400

    Define right Kleisli composition of right modules.

--- src/Control/Monad/Module.hs
+++ src/Control/Monad/Module.hs
@@ -1,7 +1,14 @@
 module Control.Monad.Module
 ( RightModule(..)
+, (>=>*)
 ) where
 
 class RightModule f where
   (>>=*) :: Monad m => f m a -> (a -> m b) -> f m b
   infixl 1 >>=*
+
+
+(>=>*) :: (RightModule f, Monad m) => (a -> f m b) -> (b -> m c) -> (a -> f m c)
+f >=>* g = \x -> f x >>=* g
+
+infixl 1 >=>*
