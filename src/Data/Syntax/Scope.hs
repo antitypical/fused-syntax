a9cec32a5f35b3966040f2553e00c67bd8f243ca
Rob Rix <rob.rix@me.com>
2019-10-04 19:05:54 -0400

    Move unprefix/unprefixEither into Data.Syntax.Stack.

--- src/Data/Syntax/Scope.hs
+++ src/Data/Syntax/Scope.hs
@@ -11,9 +11,6 @@ module Data.Syntax.Scope
 , instantiate1
 , instantiate
 , instantiateEither
-  -- * Prefixes
-, unprefix
-, unprefixEither
 ) where
 
 import Control.Applicative (liftA2)
@@ -22,7 +19,6 @@ import Control.Monad ((>=>), guard)
 import Control.Monad.Trans.Class
 import Data.Function (on)
 import Data.Syntax.Functor
-import Data.Syntax.Stack
 import Data.Syntax.Var
 import GHC.Generics (Generic, Generic1)
 
@@ -85,25 +81,3 @@ instantiate f = instantiateEither (either f pure)
 
 instantiateEither :: Monad f => (Either a b -> f c) -> Scope a f b -> f c
 instantiateEither f = unScope >=> unVar (f . Left) (>>= f . Right)
-
-
--- | Unwrap a (possibly-empty) prefix of @a@s wrapping a @t@ using a helper function.
---
---   This allows us to peel a prefix of syntax, typically binders, off of a term, returning a stack of prefixing values (e.g. variables) and the outermost subterm rejected by the function.
-unprefix
-  :: (Int -> t -> Maybe (a, t)) -- ^ A function taking the 0-based index into the prefix & the current term, and optionally returning a pair of the prefixing value and the inner subterm.
-  -> t                          -- ^ The initial term.
-  -> (Stack a, t)               -- ^ A stack of prefixing values & the final subterm.
-unprefix from = unprefixEither (matchMaybe . from)
-
--- | Unwrap a (possibly-empty) prefix of @a@s wrapping a @b@ within a @t@ using a helper function.
---
---   Compared to 'unprefix', this allows the helper function to extract inner terms of a different type, for example when @t@ is a right @b@-module.
-unprefixEither
-  :: (Int -> t -> Either (a, t) b) -- ^ A function taking the 0-based index into the prefix & the current term, and returning either a pair of the prefixing value and the next inner subterm of type @t@, or the final inner subterm of type @b@.
-  -> t                             -- ^ The initial term.
-  -> (Stack a, b)                  -- ^ A stack of prefixing values & the final subterm.
-unprefixEither from = go (0 :: Int) Nil
-  where go i bs t = case from i t of
-          Left (b, t) -> go (succ i) (bs :> b) t
-          Right b     -> (bs, b)
--- src/Data/Syntax/Stack.hs
+++ src/Data/Syntax/Stack.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE DeriveTraversable #-}
 module Data.Syntax.Stack
-( Stack(..)
+( -- * Stacks
+  Stack(..)
+  -- * Prefixes
+, unprefix
+, unprefixEither
 ) where
+
+import Data.Syntax.Var (matchMaybe)
 
 data Stack a
   = Nil
@@ -16,3 +22,25 @@ instance Semigroup (Stack a) where
 
 instance Monoid (Stack a) where
   mempty = Nil
+
+
+-- | Unwrap a (possibly-empty) prefix of @a@s wrapping a @t@ using a helper function.
+--
+--   This allows us to peel a prefix of syntax, typically binders, off of a term, returning a stack of prefixing values (e.g. variables) and the outermost subterm rejected by the function.
+unprefix
+  :: (Int -> t -> Maybe (a, t)) -- ^ A function taking the 0-based index into the prefix & the current term, and optionally returning a pair of the prefixing value and the inner subterm.
+  -> t                          -- ^ The initial term.
+  -> (Stack a, t)               -- ^ A stack of prefixing values & the final subterm.
+unprefix from = unprefixEither (matchMaybe . from)
+
+-- | Unwrap a (possibly-empty) prefix of @a@s wrapping a @b@ within a @t@ using a helper function.
+--
+--   Compared to 'unprefix', this allows the helper function to extract inner terms of a different type, for example when @t@ is a right @b@-module.
+unprefixEither
+  :: (Int -> t -> Either (a, t) b) -- ^ A function taking the 0-based index into the prefix & the current term, and returning either a pair of the prefixing value and the next inner subterm of type @t@, or the final inner subterm of type @b@.
+  -> t                             -- ^ The initial term.
+  -> (Stack a, b)                  -- ^ A stack of prefixing values & the final subterm.
+unprefixEither from = go (0 :: Int) Nil
+  where go i bs t = case from i t of
+          Left (b, t) -> go (succ i) (bs :> b) t
+          Right b     -> (bs, b)
