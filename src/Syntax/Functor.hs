b12716e255ea37cb687bf2c74aa33ec7fdea52f2
Rob Rix <rob.rix@me.com>
2019-10-05 19:55:25 -0400

    Define a Syntax type class.

--- src/Syntax/Functor.hs
+++ src/Syntax/Functor.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
 module Syntax.Functor
 ( HFunctor(..)
+, Syntax(..)
 -- * Generic deriving of 'HFunctor' instances.
 , GHFunctor(..)
 ) where
 
 import Control.Effect.Carrier (HFunctor(..), GHFunctor(..))
+
+class HFunctor sig => Syntax sig where
+  weave :: (Traversable f, Monad m, Monad n) => f () -> (forall x . f (m x) -> n (f x)) -> sig m a -> sig n (f a)
