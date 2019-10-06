d5b24e22f853bbbf0597da957a8002cab3ecfafc
Rob Rix <rob.rix@me.com>
2019-10-05 22:26:59 -0400

    Define a convenience for pretty-printing variables.

--- src/Syntax/Pretty.hs
+++ src/Syntax/Pretty.hs
@@ -6,6 +6,7 @@ module Syntax.Pretty
 , withPrec
   -- * Conveniences
 , prettyParens
+, prettyVar
 ) where
 
 data Prec = Prec
@@ -27,3 +28,8 @@ withPrec d (Prec d' a) = prettyParens (maybe False (d >) d') a
 prettyParens :: Bool -> String -> String
 prettyParens True  s = "(" <> s <> ")"
 prettyParens False s = s
+
+prettyVar :: Int -> String
+prettyVar i = alphabet !! r : if q > 0 then show q else "" where
+  (q, r) = i `divMod` 26
+  alphabet = ['a'..'z']
