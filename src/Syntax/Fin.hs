44ac55870d3bdf3609f138d8e870ca56cf130168
Rob Rix <rob.rix@me.com>
2019-10-07 19:13:46 -0400

    Define Fin.

--- src/Syntax/Fin.hs
+++ src/Syntax/Fin.hs
@@ -1,2 +1,10 @@
+{-# LANGUAGE DataKinds, GADTs, KindSignatures #-}
 module Syntax.Fin
-() where
+( Fin(..)
+) where
+
+import Syntax.Nat
+
+data Fin (n :: Nat) where
+  FZ :: Fin n
+  FS :: Fin n -> Fin ('S n)
