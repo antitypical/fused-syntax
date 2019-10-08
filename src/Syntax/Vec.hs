dfcd7f09603a50aba64b192230fcd4390fa8906d
Rob Rix <rob.rix@me.com>
2019-10-08 02:55:42 -0400

    Derive some basic instances.

--- src/Syntax/Vec.hs
+++ src/Syntax/Vec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, GADTs, KindSignatures #-}
+{-# LANGUAGE DataKinds, GADTs, KindSignatures, StandaloneDeriving #-}
 module Syntax.Vec
 ( Vec(..)
 ) where
@@ -10,3 +10,7 @@ data Vec (n :: Nat) a where
   (:#) :: a -> Vec n a -> Vec ('S n) a
 
 infixr 5 :#
+
+deriving instance Eq   a => Eq   (Vec n a)
+deriving instance Ord  a => Ord  (Vec n a)
+deriving instance Show a => Show (Vec n a)
