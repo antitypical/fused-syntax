14905c5f1b9fda3f1fe0fcd8898b81c819739e61
Rob Rix <rob.rix@me.com>
2019-10-08 02:56:13 -0400

    Define indexing into Vec.

--- src/Syntax/Vec.hs
+++ src/Syntax/Vec.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DataKinds, DeriveTraversable, GADTs, KindSignatures, StandaloneDeriving #-}
 module Syntax.Vec
 ( Vec(..)
+, (!)
 ) where
 
+import Syntax.Fin
 import Syntax.Nat
 
 data Vec (n :: Nat) a where
@@ -18,3 +20,10 @@ deriving instance Show a => Show (Vec n a)
 deriving instance Foldable    (Vec n)
 deriving instance Functor     (Vec n)
 deriving instance Traversable (Vec n)
+
+(!) :: Vec n a -> Fin n -> a
+(h :# _) ! FZ   = h
+(_ :# t) ! FS n = t ! n
+VZ       ! n    = absurd n
+
+infixl 9 !
