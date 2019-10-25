e508f4d96318add4a94877ecefa809ca845be3d8
Rob Rix <rob.rix@me.com>
2019-10-25 17:06:58 -0400

    Define HFunctor ourselves.

--- src/Syntax/Functor.hs
+++ src/Syntax/Functor.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE DefaultSignatures, QuantifiedConstraints, RankNTypes, TypeOperators #-}
 module Syntax.Functor
 ( HFunctor(..)
--- * Generic deriving of 'HFunctor' instances.
-, GHFunctor(..)
 ) where
 
-import Control.Effect.Class (HFunctor(..), GHFunctor(..))
+import Control.Effect.Class (Effect(..))
+import Control.Effect.Sum
+import Data.Functor.Identity
+
+class (forall f . Functor f => Functor (syn f)) => HFunctor syn where
+  hmap
+    :: (Monad m, Monad n)
+    => (forall a . m a -> n a)
+    -> syn m a
+    -> syn n a
+  default hmap
+    :: (Monad m, Monad n, Effect syn)
+    => (forall a . m a -> n a)
+    -> syn m a
+    -> syn n a
+  hmap f = fmap runIdentity . handle (Identity ()) (fmap Identity . f . runIdentity)
+
+instance (HFunctor l, HFunctor r) => HFunctor (l :+: r) where
+  hmap f (L l) = L (hmap f l)
+  hmap f (R r) = R (hmap f r)
