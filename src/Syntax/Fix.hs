3f50fdf02f656354b7f629520b97f0308803bf0b
Rob Rix <rob.rix@me.com>
2019-11-07 12:19:35 -0500

    Derive a Foldable instance for Fix.

--- src/Syntax/Fix.hs
+++ src/Syntax/Fix.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveFoldable, QuantifiedConstraints, StandaloneDeriving, UndecidableInstances #-}
 module Syntax.Fix
 ( Fix(..)
 ) where
 
 newtype Fix sig a = Fix { unFix :: sig (Fix sig) a }
+
+deriving instance (forall g . Foldable g => Foldable (sig g)) => Foldable (Fix sig)
