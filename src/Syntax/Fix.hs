69db56c7d66f00545d3268fd0234fbe0be2cfb16
Rob Rix <rob.rix@me.com>
2019-11-07 12:26:12 -0500

    Derive a Show instance for Fix.

--- src/Syntax/Fix.hs
+++ src/Syntax/Fix.hs
@@ -5,9 +5,10 @@ module Syntax.Fix
 
 newtype Fix sig a = Fix { unFix :: sig (Fix sig) a }
 
-deriving instance (Eq  a, forall g x . (Eq  x, forall y . Eq  y => Eq  (g y)) => Eq  (sig g x)) => Eq  (Fix sig a)
-deriving instance (Ord a, forall g x . (Eq  x, forall y . Eq  y => Eq  (g y)) => Eq  (sig g x)
-                        , forall g x . (Ord x, forall y . Ord y => Ord (g y)) => Ord (sig g x)) => Ord (Fix sig a)
+deriving instance (Eq   a, forall g x . (Eq   x, forall y . Eq   y => Eq   (g y)) => Eq   (sig g x)) => Eq   (Fix sig a)
+deriving instance (Ord  a, forall g x . (Eq   x, forall y . Eq   y => Eq   (g y)) => Eq   (sig g x)
+                         , forall g x . (Ord  x, forall y . Ord  y => Ord  (g y)) => Ord  (sig g x)) => Ord  (Fix sig a)
+deriving instance (Show a, forall g x . (Show x, forall y . Show y => Show (g y)) => Show (sig g x)) => Show (Fix sig a)
 
 deriving instance (forall g . Foldable    g => Foldable    (sig g)) => Foldable (Fix sig)
 deriving instance (forall g . Functor     g => Functor     (sig g)) => Functor  (Fix sig)
