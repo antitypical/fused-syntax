3de2240b3e889660c8312f67d3bd0b5eb681b420
Rob Rix <rob.rix@me.com>
2019-11-07 12:20:51 -0500

    Derive a Traversable instance for Fix.

--- src/Syntax/Fix.hs
+++ src/Syntax/Fix.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE DeriveFoldable, DeriveFunctor, QuantifiedConstraints, StandaloneDeriving, UndecidableInstances #-}
+{-# LANGUAGE DeriveTraversable, QuantifiedConstraints, StandaloneDeriving, UndecidableInstances #-}
 module Syntax.Fix
 ( Fix(..)
 ) where
 
 newtype Fix sig a = Fix { unFix :: sig (Fix sig) a }
 
-deriving instance (forall g . Foldable g => Foldable (sig g)) => Foldable (Fix sig)
-deriving instance (forall g . Functor  g => Functor  (sig g)) => Functor  (Fix sig)
+deriving instance (forall g . Foldable    g => Foldable    (sig g)) => Foldable (Fix sig)
+deriving instance (forall g . Functor     g => Functor     (sig g)) => Functor  (Fix sig)
+deriving instance (forall g . Foldable    g => Foldable    (sig g)
+                 , forall g . Functor     g => Functor     (sig g)
+                 , forall g . Traversable g => Traversable (sig g)) => Traversable  (Fix sig)
