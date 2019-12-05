fcedc1ae0f1881d8b0e6ea0122e47efce33dbe4d
Rob Rix <rob.rix@me.com>
2019-12-05 15:14:35 -0500

    Define an HFoldable class.

--- src/Syntax/Foldable.hs
+++ src/Syntax/Foldable.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE QuantifiedConstraints, RankNTypes #-}
 module Syntax.Foldable
-() where
+( HFoldable(..)
+) where
+
+import Control.Applicative
+
+class (forall g . Foldable g => Foldable (sig g))
+   => HFoldable sig where
+  hfoldMap
+    :: (Alternative m, Monad m, Foldable g)
+    => (forall a . g a -> m a)
+    -> (sig g a -> m a)
