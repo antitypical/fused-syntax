ff3a0d6d28b1b02fc6d0477fed51d3bae1d003e9
Rob Rix <rob.rix@me.com>
2019-12-05 14:36:32 -0500

    Reformat the signature for htraverse.

--- src/Syntax/Traversable.hs
+++ src/Syntax/Traversable.hs
@@ -13,7 +13,10 @@ class ( HFunctor sig
       , forall g . Traversable g => Traversable (sig g)
       )
    => HTraversable sig where
-  htraverse :: (Monad f, Traversable g) => (forall a . g a -> f (h a)) -> sig g a -> f (sig h a)
+  htraverse
+    :: (Monad f, Traversable g)
+    => (forall a . g a -> f (h a))
+    -> (sig g a -> f (sig h a))
 
 instance (HTraversable l, HTraversable r) => HTraversable (l :+: r) where
   htraverse f = unSum (fmap L . htraverse f) (fmap R . htraverse f)
