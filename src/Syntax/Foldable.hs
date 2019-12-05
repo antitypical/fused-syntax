4cce37cd9c31ccbbfdf2f07c7da5225eec4abe3c
Rob Rix <rob.rix@me.com>
2019-12-05 15:20:48 -0500

    Define an HFoldable instance for Sum.:+:.

--- src/Syntax/Foldable.hs
+++ src/Syntax/Foldable.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE QuantifiedConstraints, RankNTypes #-}
+{-# LANGUAGE QuantifiedConstraints, RankNTypes, TypeOperators #-}
 module Syntax.Foldable
 ( HFoldable(..)
 ) where
 
 import Control.Applicative
+import qualified Syntax.Sum as Sum
 
 class (forall g . Foldable g => Foldable (sig g))
    => HFoldable sig where
@@ -11,3 +12,6 @@ class (forall g . Foldable g => Foldable (sig g))
     :: (Alternative m, Monad m, Foldable g)
     => (forall a . g a -> m a)
     -> (sig g a -> m a)
+
+instance (HFoldable l, HFoldable r) => HFoldable (l Sum.:+: r) where
+  hfoldMap f = Sum.unSum (hfoldMap f) (hfoldMap f)
