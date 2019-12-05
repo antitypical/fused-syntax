6dbbf88a153dba50a5258011022d554887e58177
Rob Rix <rob.rix@me.com>
2019-12-05 14:14:25 -0500

    Define an HTraversable class.

--- src/Syntax/Traversable.hs
+++ src/Syntax/Traversable.hs
@@ -1,2 +1,14 @@
+{-# LANGUAGE RankNTypes, QuantifiedConstraints #-}
 module Syntax.Traversable
-() where
+( HTraversable(..)
+) where
+
+import Syntax.Functor
+
+class ( HFunctor sig
+      , forall g . Foldable g    => Foldable    (sig g)
+      , forall g . Functor  g    => Functor     (sig g)
+      , forall g . Traversable g => Traversable (sig g)
+      )
+   => HTraversable sig where
+  htraverse :: (Monad f, Traversable g) => (forall a . g a -> f (h a)) -> sig g a -> f (sig h a)
