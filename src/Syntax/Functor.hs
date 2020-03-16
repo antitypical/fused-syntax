58ebbf28ae3cfb5902dd4262e7764a29f3c795a5
Rob Rix <rob.rix@me.com>
2020-03-16 10:56:14 -0400

    Define an HFunctor instance for sums.

--- src/Syntax/Functor.hs
+++ src/Syntax/Functor.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
@@ -5,5 +6,13 @@ module Syntax.Functor
 ( HFunctor(..)
 ) where
 
+import Control.Effect.Sum ((:+:)(..))
+
 class (forall f . Functor f => Functor (h f)) => HFunctor h where
   hmap :: Functor f => (forall x . f x -> g x) -> (h f a -> h g a)
+
+instance (HFunctor l, HFunctor r) => HFunctor (l :+: r) where
+  hmap f = \case
+    L l -> L (hmap f l)
+    R r -> R (hmap f r)
+  {-# INLINE hmap #-}
